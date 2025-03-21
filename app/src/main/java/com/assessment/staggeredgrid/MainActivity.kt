e5189ddb9e34f07288926a1bcd22f5be9a84c6ad
ezge <ezge.rea@gmail.com>
2025-03-21 11:21:53 +0100

    initial commit

---
+++ app/src/main/java/com/assessment/staggeredgrid/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.assessment.staggeredgrid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.assessment.staggeredgrid.ui.theme.StaggeredGridTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StaggeredGridTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier) {
+ 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StaggeredGridTheme {
+        MainScreen()
+    }
+}
